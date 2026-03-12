4bf72fd4d6acbb36f6aba2e2e4932c1bee7d3d95
Crazy Fahim <ahmedfhm015@gmail.com>
2026-03-12 21:16:17 +0600

    Success in google.com

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# cargo build
+# RUST_LOG=info ./target/debug/rusty-wolff
+
+#!/bin/bash
+
+RUST_LOG=info cargo run
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 use anyhow::Result;
-
+use std::process;
 
 #[tokio::main]
 
-async fn main()-> Result<()> {
-    println!("Hello, world!");
+async fn main(){
+    match  try_main().await{
+        Ok(_) => {
+            log::info!("Finished!");
+        },
+        Err(e) => {
+            log::error!("Cooked! {:?}", e);
+            process::exit(-1);
+
+        }
+    }
+}
+
+async fn try_main()-> Result<()> {
+    env_logger::init();
+
+    let resp = reqwest::get("https://google.com")
+        .await?;
+    println!("{:?}", resp.text().await);
+
+    log::info!("Hello, World!");
     Ok(())
 }
+
+
